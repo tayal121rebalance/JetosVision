d26a642e9ed41876153bbfc276ad3aff919fc8c5
arafatk <arafat.da.khan@gmail.com>
2023-05-10 20:20:07 +0000

    Bare bones working search and stream fast

--- src/components/AskMe/index.js
+++ src/components/AskMe/index.js
@@ -13,59 +13,96 @@ import {
 
 import AnswerList from "../AnswerList";
 
-const openAiCompletion = async (messages, onText) => {
+const openAiCompletion = async (userQuery, messages, onText) => {
   try {
-    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+    const apiKey = process.env.REACT_APP_API_KEY;
+    let answer = "";
+
+    const queery = await fetch("/query", {
       method: "POST",
       headers: {
-        Authorization: `Bearer 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`,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        messages,
-        model: "gpt-4",
-        max_tokens: 2048,
-        stream: true,
+        query: userQuery, // Use the userQuery parameter instead of the hard-coded value
       }),
-    });
-
-    const decoder = new TextDecoder("utf8");
-    const reader = response.body.getReader();
-
-    let fullText = "";
-    let lastFire = 0;
-
-    async function read() {
-      const { value, done } = await reader.read();
-
-      if (done) return onText(fullText);
-
-      const delta = decoder
-        .decode(value)
-        .match(/"delta":\s*({.*?"content":\s*".*?"})/)?.[1];
-
-      if (delta) {
-        const content = JSON.parse(delta).content;
-
-        fullText += content;
-
-        //Detects punctuation, if yes, fires onText once per .5 sec
-        if (/[\p{P}\p{S}]/u.test(content)) {
-          const now = Date.now();
-
-          if (now - lastFire > 500) {
-            lastFire = now;
-            onText(fullText);
-          }
+    })
+      .then((queery) => {
+        if (queery.ok) {
+          return queery.json();
+        } else {
+          console.log("done");
         }
-      }
+      })
+      .then(async (data) => {
+        console.log("Response data:", data);
+        answer = data.answer; // Extract the "answer" value and assign it to the variable
+        console.log("mine Answer:", answer);
 
-      await read();
-    }
+        const secMessages = [
+          { role: "system", content: "You are a helpful assistant." },
+          { role: "user", content: answer },
+        ];
 
-    await read();
+        const response = await fetch("https://api.openai.com/v1/chat/completions", {
+          method: "POST",
+          headers: {
+            Authorization: apiKey,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            messages: secMessages,
+            model: "gpt-4",
+            max_tokens: 7048,
+            temperature: 1,
+            stream: true,
+          }),
+        });
+        if (!response.ok) {
+         const errorText = await response.text();
+          console.error("OpenAI API error:", errorText);
+        throw new Error("OpenAI API error");
+        }
 
-    return fullText;
+        const decoder = new TextDecoder("utf8");
+        const reader = response.body.getReader();
+
+        let fullText = "";
+        let lastFire = 0;
+
+        async function read() {
+          const { value, done } = await reader.read();
+
+          if (done) return onText(fullText);
+
+          const delta = decoder
+            .decode(value)
+            .match(/"delta":\s*({.*?"content":\s*".*?"})/)?.[1];
+
+          if (delta) {
+            const content = JSON.parse(delta).content;
+
+            fullText += content;
+
+            // Detects punctuation, if yes, fires onText once per .5 sec
+            if (/[\p{P}\p{S}]/u.test(content)) {
+              const now = Date.now();
+
+              if (now - lastFire > 50) {
+                lastFire = now;
+                onText(fullText);
+              }
+            }
+          }
+
+          await read();
+        }
+
+        await read();
+
+        return fullText;
+      });
+
   } catch (error) {
     return error;
   }
@@ -77,7 +114,6 @@ export default function AskMe() {
   const [data, setData] = useState([]);
 
   const askRequest = async () => {
-    
     const messages = [
       { role: "system", content: "You are a helpful assistant." },
       { role: "user", content: text },
@@ -88,7 +124,7 @@ export default function AskMe() {
       setData([...data, { question: text, answer: newText }]);
     };
 
-    await openAiCompletion(messages, onText);
+    await openAiCompletion(text, messages, onText); // Pass the text variable as an argument
     setText(" ");
   };
 
